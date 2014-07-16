9d983e15ab6c364adb6cfc1715f571c372be9b0c
Rastaman72 <m.niemiec@live.com>
2014-07-16 11:12:19 +0200

    init commit

---
+++ SpeedReader/SpeedReader/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SpeedReader
+//
+//  Created by LGBS dev on 7/16/14.
+//  Copyright (c) 2014 LGBS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SpeedReader/SpeedReader/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SpeedReader
+//
+//  Created by LGBS dev on 7/16/14.
+//  Copyright (c) 2014 LGBS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
